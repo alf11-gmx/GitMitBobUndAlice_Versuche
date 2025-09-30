d2d2546efb61bbe6b828043b4345320d788f8c57
Alfred <trump@whitehouse.org>
2025-09-30 11:42:56 +0200

    main:1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,1 @@
-﻿Console.WriteLine(":. Git mit Bob und Alice .:");
-Console.WriteLine("bob/feat1:b1");
-Console.WriteLine("alice/feat2:a1");
-Console.WriteLine("alice/feat2:a2");
+﻿Console.WriteLine(":. Git mit Bob und Alice - VERSUCHE .:");
